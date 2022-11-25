bb349addf031ec31605792695624b7902564fcf6
skyconnor30 <43676617+skyconnor30@users.noreply.github.com>
2022-11-25 08:50:04 +0700

    Add files via upload

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import {Navbar,Container,Nav,Dropdown} from 'react-bootstrap';
+import SecantCSS from "./RootofEquation/headder.module.css"
 
-var Brandstyle ={
-    color: '#61dafb',
-    fontSize: '120%',
-    fontFamily: 'Bakbak One',
-};
-var Homebar ={
-    color: 'White',
-};
+// var Brandstyle ={
+//     color: '#61dafb',
+//     fontSize: '120%',
+//     fontFamily: 'Bakbak One',
+// };
+// var Homebar ={
+//     color: 'White',
+// };
 export const NavBar = () =>{
     return(
         <Navbar bg = "dark" variant='dark' sticky='top' expand = "md" href="/">
             <Container>
                 <Navbar.Brand>
                     <img src = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" alt="" height="20"></img>
-                <babel style={Brandstyle} > Kornmongkhon Ruesingpisch Project</babel>
+                <babel  className={SecantCSS.Brandstyle}> Kornmongkhon Ruesingpisch Project</babel>
                 </Navbar.Brand>
                     <Nav className='home'>
-                        <Nav.Link href="/Home" style={Homebar}>Home</Nav.Link>
+                        <Nav.Link href="/Home" className={SecantCSS.Homebar}>Home</Nav.Link>
 
                     <Dropdown className="RootofEquation">
                         <Dropdown.Toggle variant="dark" id="dropdown-basic">
